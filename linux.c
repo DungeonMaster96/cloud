3d61b468e6e5f949817c915d12686d5230615847
Aryan Kumar <97091181+DungeonMaster96@users.noreply.github.com>
2024-07-18 06:52:17 +0530

    Update linux.c

--- linux.c
+++ linux.c
@@ -10,3 +10,13 @@ rmdir file2
 df -h
 cat file10.txt
 ln -s file1 file2
+// To search for the string "hello" in a file named file.txt:
+grep "hello" file.txt
+
+// The touch command is the simplest way to create an empty file.
+touch filename
+
+// echo txt in file.txt
+echo "hello">file.txt
+// print
+cat file.txt 
